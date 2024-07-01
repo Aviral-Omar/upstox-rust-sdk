304720581dbfd58a83ed871968133b877a8f092a
Aviral-Omar <aviralomar0301@gmail.com>
2024-07-01 22:11:27 +0530

    feat: added apis to get ws authorized urls

--- src/ws_client.rs
+++ src/ws_client.rs
@@ -3,6 +3,7 @@ use {
         client::ApiClient,
         constants::{WS_MARKET_DATA_FEED_AUTHORIZE_ENDPOINT, WS_PORTFOLIO_FEED_AUTHORIZE_ENDPOINT},
         models::{
+            error_response::ErrorResponse,
             success_response::SuccessResponse,
             ws::{
                 market_data_feed_message::{MarketDataFeedMessage, MessageData, MethodType},
@@ -118,6 +119,60 @@ where
         update_types: Option<HashSet<PortfolioUpdateType>>,
         callback: Option<F>,
     ) -> Result<JoinHandle<()>, String> {
+        let authorized_url: String = self
+            .get_authorized_portfolio_feed_endpoint(update_types)
+            .await
+            .map_err(|_| "Failed to fetch Portfolio Feed WS URL".to_string())?
+            .data
+            .authorized_redirect_uri;
+
+        let config: ClientConfig = ClientConfig::new(Url::parse(&authorized_url).unwrap());
+        let (handle, future) =
+            ezsockets::connect(|handle| PortfolioFeedClient { handle, callback }, config).await;
+        self.portfolio_feed_client = Some(handle);
+
+        let feed_future: JoinHandle<()> = tokio::spawn(async move {
+            future.await.unwrap();
+        });
+        Ok(feed_future)
+    }
+
+    pub async fn connect_market_data_feed(
+        &mut self,
+        callback: Option<G>,
+    ) -> Result<JoinHandle<()>, String> {
+        let authorized_url: String = self
+            .get_authorized_market_data_feed_endpoint()
+            .await
+            .map_err(|_| "Failed to fetch Market Data Feed WS URL".to_string())?
+            .data
+            .authorized_redirect_uri;
+
+        let config: ClientConfig = ClientConfig::new(Url::parse(&authorized_url).unwrap());
+        let (handle, future) =
+            ezsockets::connect(|handle| MarketDataFeedClient { handle, callback }, config).await;
+        self.market_data_feed_client = Some(handle);
+
+        let feed_future: JoinHandle<()> = tokio::spawn(async move {
+            future.await.unwrap();
+        });
+        Ok(feed_future)
+    }
+
+    pub async fn send_market_data_feed_message(
+        &self,
+        market_data_feed_message: MarketDataCall,
+    ) -> Result<(), EzError> {
+        if let Some(client) = &self.market_data_feed_client {
+            client.call(market_data_feed_message)?;
+        }
+        Ok(())
+    }
+
+    pub async fn get_authorized_portfolio_feed_endpoint(
+        &self,
+        update_types: Option<HashSet<PortfolioUpdateType>>,
+    ) -> Result<SuccessResponse<AuthorizeFeedResponse>, ErrorResponse> {
         let update_types: String = match update_types {
             Some(types) => {
                 if types.is_empty() {
@@ -142,62 +197,30 @@ where
                 Some(&vec![("update_types".to_string(), update_types)]),
             )
             .await;
-        if res.status().as_u16() != 200 {
-            return Err("Failed to fetch Portfolio Feed WS URL".to_string());
+
+        match res.status().as_u16() {
+            200 => Ok(res
+                .json::<SuccessResponse<AuthorizeFeedResponse>>()
+                .await
+                .unwrap()),
+            _ => Err(res.json::<ErrorResponse>().await.unwrap()),
         }
-        let authorized_url: String = res
-            .json::<SuccessResponse<AuthorizeFeedResponse>>()
-            .await
-            .unwrap()
-            .data
-            .authorized_redirect_uri;
-
-        let config: ClientConfig = ClientConfig::new(Url::parse(&authorized_url).unwrap());
-        let (handle, future) =
-            ezsockets::connect(|handle| PortfolioFeedClient { handle, callback }, config).await;
-        self.portfolio_feed_client = Some(handle);
-
-        let feed_future: JoinHandle<()> = tokio::spawn(async move {
-            future.await.unwrap();
-        });
-        Ok(feed_future)
     }
 
-    pub async fn connect_market_data_feed(
-        &mut self,
-        callback: Option<G>,
-    ) -> Result<JoinHandle<()>, String> {
+    pub async fn get_authorized_market_data_feed_endpoint(
+        &self,
+    ) -> Result<SuccessResponse<AuthorizeFeedResponse>, ErrorResponse> {
         let res: reqwest::Response = self
             .get(WS_MARKET_DATA_FEED_AUTHORIZE_ENDPOINT, true, None)
             .await;
-        if res.status().as_u16() != 200 {
-            return Err("Failed to fetch Market Data Feed WS URL".to_string());
-        }
-        let authorized_url: String = res
-            .json::<SuccessResponse<AuthorizeFeedResponse>>()
-            .await
-            .unwrap()
-            .data
-            .authorized_redirect_uri;
-        let config: ClientConfig = ClientConfig::new(Url::parse(&authorized_url).unwrap());
-        let (handle, future) =
-            ezsockets::connect(|handle| MarketDataFeedClient { handle, callback }, config).await;
-        self.market_data_feed_client = Some(handle);
 
-        let feed_future: JoinHandle<()> = tokio::spawn(async move {
-            future.await.unwrap();
-        });
-        Ok(feed_future)
-    }
-
-    pub async fn send_market_data_feed_message(
-        &self,
-        market_data_feed_message: MarketDataCall,
-    ) -> Result<(), EzError> {
-        if let Some(client) = &self.market_data_feed_client {
-            client.call(market_data_feed_message)?;
+        match res.status().as_u16() {
+            200 => Ok(res
+                .json::<SuccessResponse<AuthorizeFeedResponse>>()
+                .await
+                .unwrap()),
+            _ => Err(res.json::<ErrorResponse>().await.unwrap()),
         }
-        Ok(())
     }
 }
 
