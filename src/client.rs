81e056c7f1920c89984ad3c26d72918732b6f8a4
Aviral-Omar <aviralomar0301@gmail.com>
2024-07-08 11:12:02 +0530

    fix: Wrong documentation

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,7 @@
 //!
 //! The code below represents an API client that connects websockets and uses handlers to handle incoming data.
 //!
+//!
 //! ```rust
 //! use {
 //!     dotenvy::dotenv,
@@ -20,22 +21,22 @@
 //!         ws_client::MarketDataCall,
 //!     },
 //! };
-
+//!
 //! #[tokio::main]
 //! async fn main() {
 //!     tracing_subscriber::fmt::init();
 //!     let _ = dotenv();
-
+//!
 //!     let portfolio_feed_handler = |data: PortfolioFeedResponse| {
 //!         println!("{:?}", data);
 //!     };
 //!     let market_data_feed_handler = |data: MarketDataFeedResponse| {
 //!         println!("{:?}", data);
 //!     };
-
+//!
 //!     let api_key: String = env::var(UPLINK_API_KEY_ENV).unwrap();
 //!     let (fetch_instruments, schedule_refresh_instruments) = (false, false);
-
+//!
 //     // ApiClient with websockets connected and handler specified
 //!     let (api_client, tasks_vec) = ApiClient::new(
 //!         &api_key,
@@ -68,7 +69,7 @@
 //!     )
 //!     .await
 //!     .unwrap();
-
+//!
 //!     let api_client = api_client.lock().await;
 //!     api_client
 //!         .send_market_data_feed_message(MarketDataCall::SubscribeInstrument(MessageData {
@@ -82,27 +83,28 @@
 //!         }))
 //!         .await
 //!         .unwrap();
-
+//!
 //!     // This ensures that app continues running until the websockets die if connected or until SIGINT occurs
 //!     tokio::select! {
 //!         _ = join_all(tasks_vec) => {}
 //!         _ = signal::ctrl_c() => {}
 //!     };
 //! }
-
+//!
 //! ```
 //!
 //! The code below represents an API client that logs user in, allowing usage of authorised endpoints, automates the login process including fetching OTP via GMail and scheduling it daily.
 //!
+//!
 //! ```rust
-
+//!
 //! #[tokio::main]
 //! async fn main() {
 //!     let _ = dotenv();
-
+//!
 //!     let api_key: String = env::var(UPLINK_API_KEY_ENV).unwrap();
 //!     let (fetch_instruments, schedule_refresh_instruments) = (false, false);
-
+//!
 //!     // ApiClient which logs in automatically and schedules relogin daily when token expires
 //!     let (api_client, tasks_vec) = ApiClient::new(
 //!         &api_key,
@@ -111,7 +113,7 @@
 //!             automate_login_config: Some(AutomateLoginConfig {
 //!                 // geckodriver or chromedriver binary must be running locally with port specified in env to use automatic login or schedule login.
 //!                 // ./geckodriver --binary "~/.local/share/flatpak/exports/bin/org.mozilla.firefox" --profile-root "~/.var/app/org.mozilla.firefox/cache/mozilla/firefox/cv70hco5.default-release"
-
+//!
 //!                 // Either GOOGLE_AUTHORIZATION_CODE environment must not be set or must be recent if using for the first time or refresh_token.txt has been deleted
 //!                 automate_login: true,
 //!                 // Relogin is scheduled at 3:30 AM IST daily.
@@ -133,13 +135,13 @@
 //!     )
 //!     .await
 //!     .unwrap();
-
+//!
 //!     // This ensures that app continues running until SIGINT occurs
 //!     tokio::select! {
 //!         _ = signal::ctrl_c() => {}
 //!     };
 //! }
-
+//!
 //! ```
 //!
 //! The code below represents an API client that fetches the complete list of instruments and stores it in the API Client. It also schedules daily refresh of the list.
@@ -149,29 +151,25 @@
 //! async fn main() {
 //!     tracing_subscriber::fmt::init();
 //!     let _ = dotenv();
-
+//!
 //!     let api_key: String = env::var(UPLINK_API_KEY_ENV).unwrap();
-//!     let (fetch_instruments, schedule_refresh_instruments) = (false, false);
-
-//!     // ApiClient which logs in automatically and schedules relogin daily when token expires
+//!     let (fetch_instruments, schedule_refresh_instruments) = (true, true);
+//!
+//!     // ApiClient which fetches instruments, schedules instruments refresh daily and stores it in ApiClient
 //!     let (api_client, tasks_vec) = ApiClient::new(
 //!         &api_key,
 //!         LoginConfig {
-//!             authorize: true,
+//!             authorize: false,
 //!             automate_login_config: Some(AutomateLoginConfig {
-//!                 // geckodriver or chromedriver binary must be running locally with port specified in env to use automatic login or schedule login.
-//!                 // ./geckodriver --binary "~/.local/share/flatpak/exports/bin/org.mozilla.firefox" --profile-root "~/.var/app/org.mozilla.firefox/cache/mozilla/firefox/cv70hco5.default-release"
-
-//!                 // Either GOOGLE_AUTHORIZATION_CODE environment must not be set or must be recent if using for the first time or refresh_token.txt has been deleted
-//!                 automate_login: true,
-//!                 // Relogin is scheduled at 3:30 AM IST daily.
-//!                 schedule_login: true,
-//!                 // Fetch OTP automatically from inbox by providing email IMAP access using env variables. For GMail, login page will be opened in your default web browsers and permission must be granted.
-//!                 automate_fetching_otp: true,
+//!                 automate_login: false,
+//!                 schedule_login: false,
+//!                 automate_fetching_otp: false,
 //!                 mail_provider: Some(MailProvider::Google),
 //!             }),
 //!         },
+//!         // Fetch all instruments data from UPSTOX and store it in the ApiClient.
 //!         fetch_instruments,
+//!         // Refresh instruments data daily at 6:30 AM.
 //!         schedule_refresh_instruments,
 //!         WSConnectConfig {
 //!             connect_portfolio_stream: false,
@@ -183,7 +181,29 @@
 //!     )
 //!     .await
 //!     .unwrap();
-
+//!
+//!     {
+//!         // Ensure that api_client mutex guard goes out of scope when no longer needed.
+//!         let api_client: MutexGuard<
+//!             ApiClient<
+//!                 Box<dyn FnMut(PortfolioFeedResponse) + Send + Sync>,
+//!                 Box<dyn FnMut(MarketDataFeedResponse) + Send + Sync>,
+//!             >,
+//!         > = api_client.lock().await;
+//!         print!(
+//!             "{:?}",
+//!             api_client
+//!                 .instruments
+//!                 .as_ref()
+//!                 .unwrap()
+//!                 .get(&ExchangeSegment::NseIndex)
+//!                 .unwrap()
+//!                 .get("INDEX")
+//!                 .unwrap()
+//!         );
+//!         std::io::stdout().flush().unwrap();
+//!     }
+//!
 //!     // This ensures that app continues running until SIGINT occurs
 //!     tokio::select! {
 //!         _ = signal::ctrl_c() => {}
@@ -220,10 +240,10 @@
 //! async fn main() {
 //!     tracing_subscriber::fmt::init();
 //!     let _ = dotenv();
-
+//!
 //!     let api_key: String = env::var(UPLINK_API_KEY_ENV).unwrap();
 //!     let (fetch_instruments, schedule_refresh_instruments) = (false, false);
-
+//!
 //!     let (api_client, tasks_vec) = ApiClient::new(
 //!         &api_key,
 //!         LoginConfig {
@@ -247,7 +267,7 @@
 //!     )
 //!     .await
 //!     .unwrap();
-
+//!
 //!     {
 //!         let api_client: MutexGuard<
 //!             ApiClient<
@@ -255,14 +275,14 @@
 //!                 Box<dyn FnMut(MarketDataFeedResponse) + Send + Sync>,
 //!             >,
 //!         > = api_client.lock().await;
-
+//!
 //!         // User Endpoints
 //!         let _profile: SuccessResponse<ProfileResponse> = api_client.get_profile().await.unwrap();
 //!         let _funds_and_margin: SuccessResponse<FundAndMarginResponse> = api_client
 //!             .get_fund_and_margin(Some(SegmentType::Sec))
 //!             .await
 //!             .unwrap();
-
+//!
 //!         // Charges Endpoints
 //!         let _charges = api_client
 //!             .get_brokerage_details(BrokerageDetailsRequest {
@@ -275,7 +295,7 @@
 //!             .await
 //!             .unwrap();
 //!     }
-
+//!
 //!     // This ensures that app continues running until SIGINT occurs
 //!     tokio::select! {
 //!         _ = join_all(tasks_vec) => {}
